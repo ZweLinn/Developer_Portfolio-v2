3eee2ce53b3281d5661a4171c28d1f8a24556f72
zwelinn <zewlinnmg@gmail.com>
2026-09-07 21:44:55 +0700

    Add dark mode support with mode-watcher

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+  import { toggleMode } from "mode-watcher";
+</script>
+
+
+<h1 class="font-bold text-6xl">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
+
+<button onclick={toggleMode} class="border-spacing-3 border-zinc-600s">Toggle theme</button>
